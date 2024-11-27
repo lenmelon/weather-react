3d9f43a5d19ad78f0943ab0976332e67b77c7dbb
lenmelon <lenmelon@gmail.com>
2024-11-27 18:20:04 +0000

    Edits made to style and updated information

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,24 @@
 import React from "react";
 import axios from "axios";
+import { ColorRing } from "react-loader-spinner";
 
-export default function Weather() {
+export default function Weather(props) {
   function handleResponse(response) {
-    alert(`The weather in New York is ${response.data.main.temp}`);
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
   }
   let apiKey = "3a94f3778290bfeee61278505dbbe51d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <ColorRing
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="color-ring-loading"
+      wrapperStyle={{}}
+      wrapperClass="color-ring-wrapper"
+      colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+    />
+  );
 }
