7eaf0701621fc43ea4afe06eb3d87c217759356c
lenmelon <lenmelon@gmail.com>
2024-11-29 15:28:47 +0000

    Changes made for homework

--- src/App.js
+++ src/App.js
@@ -3,15 +3,61 @@ import Weather from "./Weather";
 
 import "./App.css";
 
-function App() {
+import "./App.css";
+
+export default function App() {
+  let weatherData = {
+    city: "New York",
+    date: "Tuesday, 17:14h",
+    description: "Rain",
+    img: "https://ssl.gstatic.com/onebox/weather/64/rain.png",
+    humidity: "80",
+    wind: "10",
+    temperature: "19",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Madrid" />
-      </header>
+      <div className="container">
+        <div className="weather-app">
+          <form className="search-form">
+            <input
+              type="text"
+              placeholder="Look for another city"
+              className="search-input"
+            />
+            <input type="submit" value="Search" className="search-button" />
+          </form>
+          <small>Last updated: {weatherData.date}</small>
+          <div className="weather-info">
+            <div className="weather-main">
+              <h1>{weatherData.city}</h1>
+              <h3 className="weather-description">{weatherData.description}</h3>
+
+              <div className="weather-temperature">
+                <img
+                  src={weatherData.img}
+                  alt="weather icon"
+                  className="weather-img"
+                />
+                <h2>
+                  {weatherData.temperature} <span>ºC</span> | <span>ºF</span>
+                </h2>
+              </div>
+            </div>
+            <div className="weather-details">
+              <ul>
+                <li>
+                  Humidity: <span>{weatherData.humidity}%</span>
+                </li>
+                <li>
+                  Wind: <span>{weatherData.wind} km/h</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
